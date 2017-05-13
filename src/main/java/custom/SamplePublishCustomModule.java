ed8c3f897ee164ea75198c5aca97886d8d221cee
jayesh <sharma.jayesh52@gmail.com>
2017-05-13 17:25:16 +0530

    wip

--- src/main/java/custom/SamplePublishCustomModule.java
+++ src/main/java/custom/SamplePublishCustomModule.java
@@ -3,6 +3,11 @@ package custom;
 
 import morph.base.actions.Action;
 import morph.base.actions.impl.GoToFlowAction;
+import morph.base.actions.impl.PublishMessageAction;
+import morph.base.beans.simplifiedmessage.SimplifiedMessage;
+import morph.base.beans.simplifiedmessage.SimplifiedMessagePayload;
+import morph.base.beans.simplifiedmessage.TextMessagePayload;
+import morph.base.beans.variables.BotContext;
 import morph.base.modules.Module;
 import org.springframework.stereotype.Service;
 
@@ -22,9 +27,17 @@ public class SamplePublishCustomModule implements Module {
     }
 
     @Override
-    public List<Action> execute() {
+    public List<Action> execute(BotContext context) {
         ArrayList<Action> actions = new ArrayList<>();
-        actions.add(new GoToFlowAction("next", false));
+        SimplifiedMessage message = new SimplifiedMessage();
+        TextMessagePayload payload = new TextMessagePayload();
+        payload.setText("Hello " + context.getUser().getName());
+        ArrayList<SimplifiedMessagePayload> payloads = new ArrayList<>();
+        payloads.add(payload);
+        message.setPayloads(payloads);
+        PublishMessageAction e = new PublishMessageAction();
+        e.setSimplifiedMessage(message);
+        actions.add(e);
         return actions;
     }
 }
